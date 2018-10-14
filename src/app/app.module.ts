d9ec289cb667d87649a03a3f0c6f357c3b07c6dd
ray1295 <ralake95@gmail.com>
2018-10-14 12:16:31 +0100

    first commit - 1 [components]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { LoginModalComponent } from './login-modal/login-modal.component';
+import { RegisterComponent } from './register/register.component';
+import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { HomeComponent } from './home/home.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoginModalComponent,
+    RegisterComponent,
+    ResetPasswordComponent,
+    HomeComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
